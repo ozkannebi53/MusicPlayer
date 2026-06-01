a8d6d620a5dc06a409a4f623a780bce1d030eccc
ozkannebi53 <ozkannebi53@gmail.com>
2026-06-01 19:21:41 +0300

    Add Song data model

---
+++ app/src/main/kotlin/com/ozkannebi53/musicplayer/models/Song.kt
@@ -0,0 +1,12 @@
+package com.ozkannebi53.musicplayer.models
+
+data class Song(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val album: String,
+    val duration: Long,
+    val path: String,
+    val albumArtUri: String? = null,
+    val isFavorite: Boolean = false
+)
